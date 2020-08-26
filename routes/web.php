477ed0c65c66fd479b829c6e0f9b3bfa659348c3
PipoLucido <monntedeoca@hotmail.com>
2020-08-26 10:37:46 -0300

    Automatic Login/Register LAravel

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,6 @@ Route::post('/updatedata','ControladorTest@updateDataBase');
 
 //entidad y relacion
 Route::get('/entidadrelacion','ControladorTest@entidadYrelacion');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
